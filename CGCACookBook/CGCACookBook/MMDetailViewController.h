5aa54750f363a4992aa393f1e1e1979db366791e
Mohammad Abdurraafay <mohammad.abdurraafay@mutualmobile.com>
2013-02-20 15:03:14 +0530

    First Checkin after basic project setup

---
+++ CGCACookBook/CGCACookBook/MMDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMDetailViewController.h
+//  CGCACookBook
+//
+//  Created by Mohammad Abdurraafay on 20/02/13.
+//  Copyright (c) 2013 Mohammad Abdurraafay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMDetailViewController : UIViewController
+
+@end
